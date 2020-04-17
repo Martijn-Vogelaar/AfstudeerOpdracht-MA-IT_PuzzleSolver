0cd8d546652913d0817a04d6e62aaf9a87c5eef6
Martijn Vogelaar <MJ.vogelaar@student.han.nl>
2020-04-17 12:00:36 +0200

    Unneeded includes removed

--- abb_controller/include/RotateGripperServer.hpp
+++ abb_controller/include/RotateGripperServer.hpp
@@ -3,19 +3,8 @@
 
 #include <actionlib/server/simple_action_server.h>
 #include <abb_controller/RotateGripperAction.h>
-#include <gazebo_ros_link_attacher/Attach.h>
 
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
-
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
-
-#include <moveit_visual_tools/moveit_visual_tools.h>
-#include <tf/transform_datatypes.h>
 
 #define PLANNING_GROUP "manipulator"
 
--- abb_controller/include/StopRobotServer.hpp
+++ abb_controller/include/StopRobotServer.hpp
@@ -3,20 +3,9 @@
 
 // Local
 #include "abb_controller/StopRobot.h"
-#include "ros/ros.h"
-
 
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
 
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
-
-#include <moveit_visual_tools/moveit_visual_tools.h>
-#include <tf/transform_datatypes.h>
 
 #define PLANNING_GROUP "manipulator"
 
